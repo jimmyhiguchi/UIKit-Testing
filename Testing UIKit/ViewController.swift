b5239ea93a8aedb6cac6d538ee4bf4b80c03bf19
User <user@users-mac.local>
2017-07-24 18:06:39 -0700

    Unit 3/ Lesson 2/Project 6 Adding days to date picker app - FAILING

--- Testing UIKit/ViewController.swift
+++ Testing UIKit/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var startDate: UIDatePicker!
+    
+    @IBOutlet weak var stepperLabel: UILabel!
+    @IBOutlet weak var newDate: UITextField!
+    
+    
+    @IBAction func updateStepper(_ sender: UIStepper) {
+        stepperLabel.text = String((Int(sender.value)))
+    }
+    
+    @IBAction func CalculateNewDate(_ sender: Any) {
+        // stuck trying to add days to the date picker
+        let date1 = startDate
+        date1.date = startDate.date.addingTimeInterval(7 * 60 * 60 * 24)
+        
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        
+        newDate.text = dateFormatter.string(for: startDate)
+    
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
